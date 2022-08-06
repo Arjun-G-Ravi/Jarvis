4f907fa039d767bd305360bf9881499b8c331e33
Arjun-G-Ravi <96014619+Arjun-G-Ravi@users.noreply.github.com>
2022-08-07 02:03:28 +0530

    Update JARVIS.py

--- JARVIS.py
+++ JARVIS.py
@@ -7,13 +7,21 @@ import wikipedia
 import os 
 import sys
 import math
-import time
+from time import sleep
 import random
 from datetime import datetime
 
                              # importing user defined modules
 from speech import *      
 
+
+                            # Some parameters
+dt=.3   #delay time
+
+
+
+
+                            #Program starts here
 print()
 print("#################################################################")
 print("*****************************************************************")
@@ -26,10 +34,28 @@ greet()
 speak("I am Jarvis. Your personal digital assistant")
 while True:
     write("How may I help you?")
+    sleep(dt)
     print("1 Interactive mode")
     print("2 Guess Game")
     print("3 Applications")
     print("5 Credits")
     print("6 Exit")
+    sleep(dt)
+    print()
+    write("Type in a choice: ")
+    x=int(input)
 
-    
+    if x==1:
+        pass
+    elif x==2:
+        pass
+    elif x==3:
+        pass
+    elif x==4:
+        pass
+    elif x==5:
+        pass
+    else:
+        pass
+
+
