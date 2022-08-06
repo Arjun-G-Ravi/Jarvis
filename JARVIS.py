7a685993dad2944ef16929d119f8d7b59891a817
Arjun-G-Ravi <96014619+Arjun-G-Ravi@users.noreply.github.com>
2022-08-07 01:38:45 +0530

    first commit
    put up all modules and all

---
+++ JARVIS.py
@@ -0,0 +1,21 @@
+import pyttsx3               # importing modules
+import webbrowser 
+import random 
+import speech_recognition as sr 
+import wikipedia
+import os 
+import sys
+import math
+import time
+import random
+from datetime import datetime
+
+import speech                # importing user defined modules
+
+print()
+print("#################################################################")
+print("*****************************************************************")
+print("                          JARVIS MARK V")
+print("*****************************************************************")
+print("#################################################################")
+print()
